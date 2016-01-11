adeb6fced750ae79f50b1c52d53aede7089f17e5
Nick Prokesch <nprokesch@gmail.com>
2016-01-11 14:36:19 -0600

    Move bookmarklet to examples directory

--- bookmarklet.js
+++
@@ -1,6 +0,0 @@
-(function(){
-var scriptType = typeof(type) !== 'undefined' ? type : 'javascript';
-var jsScript = document.createElement('SCRIPT');
-jsScript.type = 'text/' + scriptType;
-jsScript.src = href;
-document.getElementsByTagName('head')[0].appendChild(jsScript);}('https://dl.dropboxusercontent.com/u/1084227/scripts/my_fitness_pal_food_diary.js')
